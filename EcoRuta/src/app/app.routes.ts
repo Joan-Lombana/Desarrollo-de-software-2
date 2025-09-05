c74aee859923f2b36a234e13072954fafa08ba9e
Joan-Lombana <joan.lombana@correounivalle.edu.co>
2025-09-05 18:29:24 -0500

    creacion del proyecto

--- EcoRuta/src/app/app.routes.ts
+++ EcoRuta/src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Principal } from './pages/principal/principal';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: Inicio
+    },
+
+     {
+        path: "",
+        component: Principal
+    }
+];
